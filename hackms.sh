7439e4f0a95f2a757a5a4c387d750171f23eb2dc
choncey <nchoncey@gmail.com>
2023-12-22 11:16:17 -0800

    made script for metasploit approach

--- hackms.sh
+++ hackms.sh
@@ -1,1 +1,25 @@
-# I'll be back
+#!/bin/bash
+
+# Prompt the user for input
+read -p "Enter your target ip: " target_ip
+
+# Prompt the user for more input
+read -p "Did you run nmap scan?"
+
+if [ "nmap scan"="y" ] then
+	run 'msfconsole'
+fi
+
+# Prompt user for input
+read -p "set RHOSTS"
+
+# Prompt for input
+read -p "set LHOST"
+
+# Prompt for input
+read -p "set RPORT"
+
+# Prompt for input
+read -p "set payload"
+
+Run "Exploit" command
